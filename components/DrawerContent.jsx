061db111fd6405ef224201f8e0b20e22142f01f4
rajnikanth-ekayaa <rajnikanth@ekayaa.co.in>
2024-06-24 20:54:10 +0530

    fixed the look n feel of drawer content

--- components/DrawerContent.jsx
+++ components/DrawerContent.jsx
@@ -3,11 +3,16 @@ import { View, Text, Image, StyleSheet, Linking } from "react-native";
 import { DrawerContentScrollView, DrawerItem, DrawerItemList } from "@react-navigation/drawer";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { ImageAssets } from "../assets/Assets";
-import { Navigation } from "lucide-react-native";
-import AppStore from "../src/stores/AppStore";
+import { Navigation, User } from "lucide-react-native";
 import appStoreInstance from "../src/stores/AppStore";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Avatar, Box, Divider, HStack, VStack } from "@gluestack-ui/themed";
+import { Icon } from "@gluestack-ui/themed";
+import { Ionicons } from "@expo/vector-icons";
 
 const DrawerContent = (props) => {
+	console.log("DrawerContent: ", props.state);
+
 	const handleLogout = () => {
 		// Implement your logout logic here
 		console.log("User logged out");
@@ -17,26 +22,39 @@ const DrawerContent = (props) => {
 	};
 	return (
 		<DrawerContentScrollView {...props}>
-			<View style={styles.profileContainer}>
-				<Image
-					source={ImageAssets.logo} // Replace with your profile image URL
-					style={styles.profileImage}
-				/>
-				<Text style={styles.profileName}>Mudit Dixit</Text>
-			</View>
-			<DrawerItemList {...props} />
-			<DrawerItem label='Case Log' onPress={() => props.navigation.navigate("RootLogBook")} />
-			<DrawerItem label='Resources' onPress={() => props.navigation.navigate("Resources")} />
-			<DrawerItem label='Community' onPress={() => props.navigation.navigate("Community")} />
-			<TouchableOpacity
-				onPress={() => {
-					/* Handle logout */
-				}}
-				style={styles.logoutButton}>
-				<Text onPress={handleLogout} style={styles.logoutText}>
-					Logout
-				</Text>
-			</TouchableOpacity>
+			<VStack alignItems='center' py='$8'>
+				<Avatar bgColor='#357A71'>
+					{/* User is imported from 'lucide-react-native' */}
+					<Icon as={User} color='white' size='lg' />
+				</Avatar>
+				<Text style={styles.profileName}>{appStoreInstance.UserName}</Text>
+			</VStack>
+			<Divider />
+			<DrawerItem
+				icon={({ focused, color, size }) => <Ionicons name='home' size={size} color={color} />}
+				label='Home'
+				onPress={() => props.navigation.navigate("Dashboard")}
+			/>
+			<DrawerItem
+				icon={({ focused, color, size }) => <Ionicons name='document-text' size={size} color={color} />}
+				label='Case Log'
+				onPress={() => props.navigation.navigate("RootLogBook")}
+			/>
+			<DrawerItem
+				icon={({ focused, color, size }) => <Ionicons name='play-circle-outline' size={size} color={color} />}
+				label='Resources'
+				onPress={() => props.navigation.navigate("Resources")}
+			/>
+			<DrawerItem
+				icon={({ focused, color, size }) => <Ionicons name='people-outline' size={size} color={color} />}
+				label='Community'
+				onPress={() => props.navigation.navigate("Community")}
+			/>
+			<DrawerItem
+				icon={({ focused, color, size }) => <Ionicons name='power-sharp' size={size} color={color} />}
+				label='Logout'
+				onPress={handleLogout}
+			/>
 		</DrawerContentScrollView>
 	);
 };
