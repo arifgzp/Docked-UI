f3724836578e7edb5665d525bd68075c5d8ee456
rajnikanth-ekayaa <rajnikanth@ekayaa.co.in>
2024-11-04 23:36:54 +0530

    Added fixes to support iOS

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			[
+				"transform-inline-environment-variables",
+				{
+					include: ["NODE_ENV", "REACT_NATIVE_MODE"],
+				},
+			],
+			"react-native-reanimated/plugin", //Reanimated plugin has to be listed last.
+		],
+	};
 };
